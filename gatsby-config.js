171dbc4ca3f984ca0ef60bc7a625f61663ca19ee
Bruce Huang <oursyida@gmail.com>
2021-03-20 23:54:29 +0800

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Bruce Huang`,
+    author: `Bruce Huang`,
+    description: `Writing blogs for fun.`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/brucehuanglove`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default function Bio() {
+  return (
+    <>
+      Bruce Huang
+      <br />
+      He / him. Front end newbie. Writing blogs for fun.
+    </>
+  )
+}
